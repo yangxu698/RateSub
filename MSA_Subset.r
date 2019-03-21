d65dc2a84bcc89461bd6508b17541c46375415da
yangxu698 <yangxu698@gmail.com>
2019-03-21 11:57:18 -0400

    debug colnames

--- MSA_Subset.r
+++ MSA_Subset.r
@@ -11,9 +11,9 @@ MSA_subset = function(j)
             summary(deposit.raw)
             deposit.raw = deposit.raw %>% filter(productcode %in% rates.array)
             branch.in.MSA = unlist(Deposit_InstitutionDetails %>% filter( MSA == j) %>% select(ACCT_NBR))
-            ncol_temp = ncol(temp)
+            nrow_temp = nrow(temp)
             temp = rbind(temp, deposit.raw %>% filter(accountnumber %in% branch.in.MSA) %>% select(accountnumber, productcode, rate, surveydate))
-            MSA_count = c(MSA_count, ncol(temp)-ncol_temp)
+            MSA_count = c(MSA_count, nrow(temp)-nrow_temp)
 
       }
   timestamp_MSA = c(j,start_time, Sys.time(), MSA_count)
