3dd1df1cc3ceab29e60310922e043a7ae9b612a3
yangxu698 <yangxu698@gmail.com>
2019-03-21 14:28:28 -0400

    debug writecsv

--- try24.r
+++ try24.r
@@ -75,6 +75,7 @@ timestamp = foreach( j = itx, .combine = 'rbind') %dopar%
                 MSA_subset(j)
 
               }
-##colnames(timestamp) = c("MSA", "start_time", "end_time", files.name.array[1:2])
-write_csv(tbl_df(timestamp), paste0("../E12core/", "timestamp",".csv"))
+timestamp = tbl_df(timestamp)
+colnames(timestamp) = c("MSA", "start_time", "end_time", files.name.array[1:2])
+write_csv(timestamp, paste0("../E12core/", "timestamp",".csv"))
 stopImplicitCluster()
