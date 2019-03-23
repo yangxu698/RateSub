19028f67a66af6beee1a3303ad8eb88aa55f268c
yangxu698 <yangxu698@gmail.com>
2019-03-22 21:23:27 -0400

    MSA file update

--- MSA.r
+++ MSA.r
@@ -56,7 +56,7 @@ files.name.array = c("depositRateData_2000_09.txt","depositRateData_2003_09.txt"
 "depositRateData_2000_07.txt","depositRateData_2003_07.txt","depositRateData_2006_07.txt","depositRateData_2009_07.txt","depositRateData_2012_07.txt","depositRateData_2015_07.txt",
 "depositRateData_2000_08.txt","depositRateData_2003_08.txt","depositRateData_2006_08.txt","depositRateData_2009_08.txt","depositRateData_2012_08.txt","depositRateData_2015_08.txt")
 files.name.array = sort(files.name.array)
-MSA = as.array(unique(Deposit_InstitutionDetails$MSA))
+MSA = Deposit_InstitutionDetails %>% select(MSA) %>% unique() %>% pull(MSA)
 length(MSA)
 
 
