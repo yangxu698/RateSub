8f9de634c6645c323b4303c448fd1fa277a26156
yangxu698 <yangxu698@gmail.com>
2019-03-24 20:54:03 -0400

    debug smarket_Subset.r

--- smarket_Subset.r
+++ smarket_Subset.r
@@ -9,13 +9,13 @@ smarket_subset = function(j)
           ## deposit.raw = deposit.raw %>% mutate_if(is.character, as.factor)
           ## summary(deposit.raw)
           deposit.raw = deposit.raw %>% filter(productcode %in% rates.array)
-          branch.in.smarket = j[1,]
+          branch.in.smarket = j[,1] %>% pull(ACCT_NBR)
           nrow_temp = nrow(temp)
           temp = rbind(temp, deposit.raw %>% filter(accountnumber %in% branch.in.smarket) %>% select(accountnumber, productcode, rate, surveydate))
           smarket_count = c(smarket_count, nrow(temp)-nrow_temp)
 
     }
   timestamp_smarket = c(j[1,2],as.character(start_time), as.character(Sys.time()), smarket_count)
-  write_csv(temp, paste0("../smarket/", "smarket", j[1,2] ,".csv"))
+  write_csv(temp, paste0("../smarket/", "smarket.", j[1,2] ,".csv"))
   return(timestamp_smarket)
 }
