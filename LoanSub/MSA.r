62abf46a539b94095dfc0dcbb03534a71f9c5e68
yangxu698 <yangxu698@gmail.com>
2019-04-04 15:50:15 -0400

    loan data subset upload

--- LoanSub/MSA.r
+++ LoanSub/MSA.r
@@ -5,8 +5,13 @@ rm(list=ls())
 library(dplyr)
 library(readr)
 ## setwd("./LoanSub")
-Loan_InstitutionDetails = read_delim("../../../loan/Loan_InstitutionDetails.txt", delim = "|")
-
+Loan_InstitutionDetails = read_delim("../../../RW_MasterHistoricalLoanData_042018/Loan_InstitutionDetails.txt", delim = "|") %>%
+      select(accountnumber = acct_nbr, inst_nm, state, city, county, branchdeposits, state_fps, cnty_fps, msa, cbsa)
+MSA = Loan_InstitutionDetails %>% pull(msa) %>% unique() %>% na.omit() %>% sort()
+CBSA = Loan_InstitutionDetails %>% pull(cbsa) %>% unique() %>% na.omit() %>% sort()
+str(MSA)
+str(CBSA)
+str(Loan_InstitutionDetails)
 ## DepNameChgHis = DepNameChgHis %>% mutate_if(is.character, as.factor)
 ## Deposit_acct_join  = Deposit_acct_join %>% mutate_if(is.character, as.factor)
 ## DepositCertChgHist = DepositCertChgHist %>% mutate_if(is.character, as.factor)
@@ -21,7 +26,7 @@ rates.array = c("1YrARM175K", "15YrFixMtg175k", "30YrFixMtg175K", "AUTONEW", "AU
 files.name.array = read_csv("loan_file_list.csv")
 
 file_list = files.name.array %>%
-              mutate(code = gsub("(^.*_)(\\w+)(_.+$)","\\2", file_list)) %>%
+              mutate(code = gsub("(^.+_)(\\w+)(_.+$)","\\2", file_list)) %>%
               filter(code %in% rates.array) %>%
               pull(file_list) %>%
               sort()
@@ -29,17 +34,22 @@ file_list = files.name.array %>%
 file_list
 ## MSA_list = read.csv("../MSAGet1.csv", stringsAsFactors = FALSE) %>%
 ##            mutate( MSA = substr(MSA,4,7)) %>% pull(MSA)
-MSA_list = read.csv("MSAGroup1.csv") %>% pull(x)
+## ARM1Yr = read_delim("/home/yang/RateWatch/UnzippedData/loan/loanRateData_1YrARM175K_2005.txt", delim = "|")
+## FixMtg15Yr = read_delim("/home/yang/RateWatch/UnzippedData/loan/loanRateData_15YrFixMtg175K_2005.txt", delim = "|")
+## FixMtg30Yr = read_delim("/home/yang/RateWatch/UnzippedData/loan/loanRateData_30YrFixMtg175K_2005.txt", delim = "|")
+## AUTONEW  = read_delim("/home/yang/RateWatch/UnzippedData/loan/loanRateData_AUTONEW_2005.txt", delim = "|")
+## AUTOUSED2YR = read_delim("/home/yang/RateWatch/UnzippedData/loan/loanRateData_AUTOUSED2YR_2005.txt", delim = "|")
+## PersonalUnsecLoan = read_delim("/home/yang/RateWatch/UnzippedData/loan/loanRateData_PersonalUnsecLoan_2005.txt", delim = "|")
 
 
 library(foreach)
 library(doParallel)
 library(iterators)
-source("MSA_Subset.r")
+source("MSAsubset.r")
 cores_number = 4
 ## timestamp = tbl_df(c())
 registerDoParallel(cores_number)
-itx = iter(MSA_list)
+itx = iter(MSA)
 itx
 timestamp = foreach(j = itx,.combine = 'rbind') %dopar%
 ## for (j in 1:length(MSA))
@@ -48,5 +58,5 @@ timestamp = foreach(j = itx,.combine = 'rbind') %dopar%
               }
 colnames(timestamp) = c("MSA", "start_time", "end_time", files.name.array)
 timestamp = tbl_df(timestamp)
-write_csv(timestamp, paste0("../../E12core/", "timestamp", as.character(Sys.time()),".csv"))
+write_csv(timestamp, paste0("../../../RW_MasterHistoricalLoanData_042018/MSA/", "timestamp", as.character(Sys.time()),".csv"))
 stopImplicitCluster()
---
+++ LoanSub/MSAsubset.r
@@ -0,0 +1,35 @@
+MSA_subset = function(j)
+{
+
+  temp = tbl_df(c())
+  MSA_count = c()
+  start_time = Sys.time()
+  productfilter =  c("1 Year ARM @ 175K - Amort","1 Year ARM @ 175K - Caps","1 Year ARM @ 175K - Dwn Pmt",
+                      "1 Year ARM @ 175K - Orig Fees","1 Year ARM @ 175K - Points", "1 Year ARM @ 175K - Rate",
+                      "15 Yr Fxd Mtg @ 175K - Dwn Pmt","15 Yr Fxd Mtg @ 175K - Orig Fees","15 Yr Fxd Mtg @ 175K - Points",
+                      "15 Yr Fxd Mtg @ 175K - Rate", "30 Yr Fxd Mtg @ 175K - Dwn Pmt", "30 Yr Fxd Mtg @ 175K - Orig Fees",
+                      "30 Yr Fxd Mtg @ 175K - Points", "30 Yr Fxd Mtg @ 175K - Rate",
+                      "Auto New - 36 Mo Term","Auto New - 60 Mo Term", "Auto Used 2 Yrs - 36 Mo Term",
+                      "Auto Used 2 Yrs - 60 Mo Term", "Personal Unsecured Loan - Tier 1")
+  ## "Auto New - % Financed", "Auto New - 48 Mo Term","Auto New - 72 Mo Term","Auto Used 2 Yrs - % Financed", "Auto Used 2 Yrs - 48 Mo Term", "Personal Unsecured Loan - Max Term",
+  ## write.csv(j, paste0("../../../MSA/", j,"starts",".csv"))
+  branch.in.MSA = unlist(Loan_InstitutionDetails %>% filter( msa == j) %>% pull(accountnumber))
+  for (i in 1:length(files.name.array))
+      {
+            deposit.raw = read_delim(paste0("../../../RW_MasterHistoricalLoanData_042018/",files.name.array[i]), delim = "|") %>%
+                          filter(prod_name %in% productfilter) %>%
+                          filter(accountnumber %in% branch.in.MSA) %>%
+                          select(accountnumber, prod_code, prod_name, date, applicablemeasurement)
+
+            ## deposit.raw = deposit.raw %>% mutate_if(is.character, as.factor)
+            ## summary(deposit.raw)
+            nrow_temp = nrow(temp)
+            temp = rbind(temp, deposit.raw)
+            MSA_count = c(MSA_count, nrow(temp)-nrow_temp)
+
+      }
+  timestamp_MSA = c(j,as.character(start_time), as.character(Sys.time()), MSA_count)
+  write_csv(temp, paste0("../../../RW_MasterHistoricalLoanData_042018/MSA/", "MSA", j ,".csv"))
+  rm(temp)
+  return(timestamp_MSA)
+}
