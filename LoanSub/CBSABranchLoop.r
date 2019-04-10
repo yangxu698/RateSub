5e8ecba86bffad54bb704de734fdbdab405a23b8
yangxu698 <yangxu698@gmail.com>
2019-04-10 11:09:58 -0400

    update MSA/CBSA branchloop filters

--- LoanSub/CBSABranchLoop.r
+++ LoanSub/CBSABranchLoop.r
@@ -39,19 +39,37 @@ CBSABranchLoop = function(j)
                ## group_by(inst_nm)
                unique()
                ## mutate(branchType = "B1")
-
-
-    branchBXX = CBSA_raw %>%
+    products_in_filter = c("1 Year ARM @ 175K - Rate",
+                          "15 Yr Fxd Mtg @ 175K - Rate",
+                          "30 Yr Fxd Mtg @ 175K - Rate",
+                          "Auto New - 36 Mo Term","Auto New - 60 Mo Term",
+                          "Auto Used 2 Yrs - % Financed",
+                          "Home E.L.O.C. up to 80% LTV - Annual Fee",
+                          "Home E.L.O.C. up to 80% LTV - Tier 1",
+                          "Home E.L.O.C. up to 80% LTV - Tier 4",
+                          "Personal Unsecured Loan - Tier 1",
+                          "Personal Unsecured Loan - Tier 4")
+    branchBXX = MSA_raw %>%
               filter(accountnumber %in% branchBX$accountnumber) %>%
-              ## mutate(date_num = as.numeric(as.POSIXct(surveydate))) %>%
               left_join(branchBX, by = "accountnumber") %>%  ## append the info: institution name and branch deposits
-              group_by(prod_code) %>%
-              group_by(accountnumber) %>%  ## grouping by accountnumber
-              mutate(survey_span = table(accountnumber)) %>%   ## calculate the survey span
-              ungroup() %>% group_by(inst_nm) %>% top_n(1, survey_span)  %>% ## grouping by institution name and select the longest survey span
+              group_by(prod_code, accountnumber) %>%
+              mutate(survey_span = table(prod_code)) %>%
+              ungroup() %>% group_by(inst_nm,prod_code) %>% top_n(1, survey_span)  %>% ## grouping by institution name and select the longest survey span
               ungroup() %>%
-              select(accountnumber, inst_nm) %>%
+              select(accountnumber, inst_nm, prod_name, survey_span) %>%
+              filter(prod_name %in% products_in_filter) %>%
               unique()
+
+    ## branchBXX = CBSA_raw %>%
+    ##           filter(accountnumber %in% branchBX$accountnumber) %>%
+    ##           left_join(branchBX, by = "accountnumber") %>%  ## append the info: institution name and branch deposits
+    ##           group_by(prod_code) %>%
+    ##           group_by(accountnumber) %>%  ## grouping by accountnumber
+    ##           mutate(survey_span = table(accountnumber)) %>%   ## calculate the survey span
+    ##           ungroup() %>% group_by(inst_nm) %>% top_n(1, survey_span)  %>% ## grouping by institution name and select the longest survey span
+    ##           ungroup() %>%
+    ##           select(accountnumber, inst_nm) %>%
+    ##           unique()
 
     branchB2 = branchBXX %>%
                group_by(inst_nm) %>%
--- LoanSub/MSABranchLoop.r
+++ LoanSub/MSABranchLoop.r
@@ -34,36 +34,41 @@ MSABranchLoop = function(j)
 
     ##   Extract institutions with possible multiple branches, no Jan.1999 data  ##
     branchBX = branchB %>% anti_join(branchB1, by="inst_nm") %>%
-               ## group_by(prod_code) %>%
                select(accountnumber, inst_nm) %>%
-               ## group_by(inst_nm)
                unique()
-               ## mutate(branchType = "B1")
 
 
+    products_in_filter = c("1 Year ARM @ 175K - Rate",
+                          "15 Yr Fxd Mtg @ 175K - Rate",
+                          "30 Yr Fxd Mtg @ 175K - Rate",
+                          "Auto New - 36 Mo Term","Auto New - 60 Mo Term",
+                          "Auto Used 2 Yrs - % Financed",
+                          "Home E.L.O.C. up to 80% LTV - Annual Fee",
+                          "Home E.L.O.C. up to 80% LTV - Tier 1",
+                          "Home E.L.O.C. up to 80% LTV - Tier 4",
+                          "Personal Unsecured Loan - Tier 1",
+                          "Personal Unsecured Loan - Tier 4")
     branchBXX = MSA_raw %>%
               filter(accountnumber %in% branchBX$accountnumber) %>%
               left_join(branchBX, by = "accountnumber") %>%  ## append the info: institution name and branch deposits
               group_by(prod_code, accountnumber) %>%
-              mutate(survey_span1 = table(prod_code)) %>%
-              ## group_by(accountnumber) %>%  ## grouping by accountnumber
-              ## mutate(survey_span = table(accountnumber)) %>%   ## calculate the survey span
-              ## ungroup() %>% group_by(inst_nm) %>% top_n(1, survey_span)  %>% ## grouping by institution name and select the longest survey span
-              ungroup() %>% group_by(inst_nm,prod_code) %>% top_n(1, survey_span1)  %>% ## grouping by institution name and select the longest survey span
+              mutate(survey_span = table(prod_code)) %>%
+              ungroup() %>% group_by(inst_nm,prod_code) %>% top_n(1, survey_span)  %>% ## grouping by institution name and select the longest survey span
               ungroup() %>%
-              select(accountnumber, inst_nm, prod_name, survey_span1) %>%
+              select(accountnumber, inst_nm, prod_name, survey_span) %>%
+              filter(prod_name %in% products_in_filter) %>%
               unique()
 
-    branchBXX1 = MSA_raw %>%
-              filter(accountnumber %in% branchBX$accountnumber) %>%
-              left_join(branchBX, by = "accountnumber") %>%  ## append the info: institution name and branch deposits
-              group_by(prod_code) %>%
-              group_by(accountnumber) %>%  ## grouping by accountnumber
-              mutate(survey_span = table(accountnumber)) %>%   ## calculate the survey span
-              ungroup() %>% group_by(inst_nm) %>% top_n(1, survey_span)  %>% ## grouping by institution name and select the longest survey span
-              ungroup() %>%
-              select(accountnumber, inst_nm, survey_span) %>%
-              unique()
+  ##   branchBXX = MSA_raw %>%
+  ##             filter(accountnumber %in% branchBX$accountnumber) %>%
+  ##             left_join(branchBX, by = "accountnumber") %>%  ## append the info: institution name and branch deposits
+  ##             group_by(prod_code) %>%
+  ##             group_by(accountnumber) %>%  ## grouping by accountnumber
+  ##             mutate(survey_span = table(accountnumber)) %>%   ## calculate the survey span
+  ##             ungroup() %>% group_by(inst_nm) %>% top_n(1, survey_span)  %>% ## grouping by institution name and select the longest survey span
+  ##             ungroup() %>%
+  ##             select(accountnumber, inst_nm, survey_span) %>%
+  ##             unique()
 
     branchB2 = branchBXX %>%
                group_by(inst_nm) %>%
