44bfba228fc4c48ac01f6d2a35aa12c7e58a8eb4
yangxu698 <yangxu698@gmail.com>
2019-04-12 14:46:33 -0400

    fun_instDepoSub debug

--- InstitutionSub/fun_instDepoSub.r
+++ InstitutionSub/fun_instDepoSub.r
@@ -2,6 +2,7 @@ inst_deposit_subset = function(j)
 {
   temp = tbl_df(c())
   start_time = Sys.time()
+  inst_count = c()
   for (i in 1:length(files.name.array))
       {
             deposit.raw = read_delim(paste0("../../RW_MasterHistoricalDepositFiles_042018/",files.name.array[i]), delim = "|")
@@ -11,11 +12,11 @@ inst_deposit_subset = function(j)
             branch.in.inst_nm = Deposit_InstitutionDetails %>% filter( INST_NM == j) %>% pull(ACCT_NBR)
             nrow_temp = nrow(temp)
             temp = rbind(temp, deposit.raw %>% filter(accountnumber %in% branch.in.inst_nm) %>% select(accountnumber, productcode, rate, surveydate))
-            MSA_count = c(MSA_count, nrow(temp)-nrow_temp)
+            inst_count = c(inst_count, nrow(temp)-nrow_temp)
 
       }
-  timestamp_MSA = c(j,as.character(start_time), as.character(Sys.time()), MSA_count)
+  timestamp_n = c(j,as.character(start_time), as.character(Sys.time()), MSA_count)
   write_csv(temp, paste0("../../InstSelect/", j, "Deposit", ".csv"))
   rm(temp)
-  return(timestamp_MSA)
+  return(timestamp_n)
 }
