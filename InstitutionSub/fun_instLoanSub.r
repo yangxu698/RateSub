c7e41a4f1d8f00495a1e77ba479a7a2d45233d0a
yangxu698 <yangxu698@gmail.com>
2019-04-12 18:50:27 -0400

    debug fun_instLoanSub.r

--- InstitutionSub/fun_instLoanSub.r
+++ InstitutionSub/fun_instLoanSub.r
@@ -9,7 +9,7 @@ inst_loan_subset = function(j)
             branch.in.inst_nm = Loan_InstitutionDetails %>% filter( inst_nm == j) %>% pull(accountnumber)
             ## deposit.raw = deposit.raw %>% mutate_if(is.character, as.factor)
             loan.raw = read_delim(paste0("../../RW_MasterHistoricalLoanData_042018/",file_list[i]), delim = "|") %>%
-                        filter(productcode %in% rates.array) %>%
+                        filter(prod_name %in% rates.array) %>%
                         filter(accountnumber %in% branch.in.inst_nm) %>%
                         select(accountnumber, prod_code, prod_name, date, applicablemeasurement)
             nrow_temp = nrow(temp)
